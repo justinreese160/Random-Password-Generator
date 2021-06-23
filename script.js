898e1e3fd60a9dfc7fbdaf1f7656c73ec87cc06e
Justin <justinreese160@gmail.com>
2021-06-23 15:15:49 -0500

    added variables, prompts, and confirms

--- script.js
+++ script.js
@@ -12,3 +12,42 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var char = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "}", "|", "~", '"', "'",];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+function generatePassword()
+{
+  var length = parseInt (prompt('How many characters would you like in your password?', 'min 8 max 128' ))
+  console.log(length)
+
+  if (isNaN(length)) {
+    var length = parseInt (prompt('How many characters would you like in your password?', 'min 8 max 128' ))  }
+  else if (length < 8 && length > 128){
+    var length = parseInt (prompt('How many characters would you like in your password?', 'min 8 max 128' ))
+  }
+  let passwordArr = []
+  var upper = confirm ("Do you want uppercase characters?")
+  if (upper === true){
+    passwordArr = upperCase
+  }
+  var lower = confirm ("Do you want lowercase characters?")
+  if (lower === true && passwordArr.length > 0){
+    passwordArr.concat(lower)
+  }
+  var special = confirm ("Do you want special characters?")
+  // if (upper === true){
+  //   passwordArr = upperCase
+  // }
+  var number = confirm ("Do you want numbers?")
+  console.log (upper,lower,special,number);
+ 
+
+
+
+ 
+}
+
+
