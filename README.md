75feec148c156272c24642dba958c7500c5e9fcd
Justin <justinreese160@gmail.com>
2021-06-24 15:17:08 -0500

    adding read me

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Random-Password-Generator
+
+Created a password generator that generates the password based on the user selecting password length and which variables the user wants to include in their password.
+
+
+
+
+## Link to application
--- script.js
+++ script.js
@@ -18,25 +18,24 @@ var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'
 var char = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "}", "|", "~", '"', "'",];
 var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-//TODO add semicolons
 function generatePassword()
 {
-  var length = parseInt (prompt('How many characters would you like in your password?', 'min 8 max 128' ))
+  var length = parseInt (prompt('How many characters would you like in your password?', 'min 8 max 128' ));
 
   if (isNaN(length)) {
-    var length = parseInt (prompt('How many characters would you like in your password?', 'min 8 max 128' ))  }
+    var length = parseInt (prompt('How many characters would you like in your password?', 'min 8 max 128' ));  }
   else if (length < 8 && length > 128){
-    var length = parseInt (prompt('How many characters would you like in your password?', 'min 8 max 128' ))
+    var length = parseInt (prompt('How many characters would you like in your password?', 'min 8 max 128' ));
   }
 
   let passwordArr = []
   //Starts asking how they want their password
-  var upper = confirm ("Do you want uppercase characters?")
+  var upper = confirm ("Do you want uppercase characters?");
   if (upper === true){
     passwordArr = upperCase
   }
   
-  var lower = confirm ("Do you want lowercase characters?")
+  var lower = confirm ("Do you want lowercase characters?");
   if (lower === true && passwordArr.length > 0){ //If they want a lowercase char and already chose uppercase char
     passwordArr = passwordArr.concat(lowerCase)
   } else if (lower) { //They did not choose uppercase char, but do want lowercase char
@@ -44,14 +43,14 @@ function generatePassword()
   }
 
 
-  var special = confirm ("Do you want special characters?")
+  var special = confirm ("Do you want special characters?");
   if (special === true && passwordArr.length > 0){ // If they want special characters and already chose an option
     passwordArr = passwordArr.concat(char)
   } else if (special) { // They did not choose above option but want special characters
     passwordArr = char
   }
   
-  var doesUserWantNumbers = confirm ("Do you want numbers?")
+  var doesUserWantNumbers = confirm ("Do you want numbers?");
   if (doesUserWantNumbers === true && passwordArr.length > 0){ //If they want numbers and already chose an option
     passwordArr = passwordArr.concat(number)
   } else if (passwordArr.length <1) { // They chose no above options, forces to choose at least 1 option
